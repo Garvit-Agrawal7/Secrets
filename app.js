5fdd0319dfa86d9ced2c2bb81d886f6b9c5e0379
Garvit-Agrawal7 <garvitagrawal75@gmail.com>
2023-09-21 10:40:29 +0530

    Level 4 Authentication using Bcrypt

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import md5 from "md5";
+import bcrypt from "bcrypt";
 
 const app = express();
+const saltRounds = 10;
 mongoose.connect("mongodb://localhost:27017/loginDB")
 	.catch(function (err) {
 		console.log(err)
@@ -14,7 +15,7 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String
 })
-
+await bcrypt.compare(password, hash)
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,27 +37,31 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-	const newUser = new User({
-		email: req.body.username,
-		password: md5(req.body.password)
-	});
-	newUser.save()
-		.then(function () {
-			res.render("secrets.ejs")
-		})
-		.catch(function (err) {
-			console.log(err)
-		})
+	bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+		const newUser = new User({
+			email: req.body.username,
+			password: hash
+		});
+		newUser.save()
+			.then(function () {
+				res.render("secrets.ejs")
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+	})
 })
 
 app.post("/login", (req, res) => {
 	const username = req.body.username
-	const password = md5(req.body.password)
+	const password = req.body.password
 	User.findOne({email:username})
 		.then(function (foundUser) {
-			if (foundUser.password === password) {
-				res.render("secrets.ejs")
-			}
+			bcrypt.compare(password, foundUser.password, (err, result) => {
+				if (result === true) {
+					res.render("secrets.ejs")
+				}
+			})
 		})
 		.catch(function (err) {
 			console.log(err)
